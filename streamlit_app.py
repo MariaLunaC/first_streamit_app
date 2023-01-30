e4991fad7def73d99ed588488de8ad622c1abbed
MariaLunaC <123782711+MariaLunaC@users.noreply.github.com>
2023-01-30 13:45:54 -0600

    Update streamlit_app.py
    Add menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,6 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
 streamlit.title('Breakfast Menu')
+streamlit.title('Omega 3 & Blueberry ')
+streamlit.title('Kale, Spinach & Rocket Smoothie')
+streamlit.title('Hard-Boiled Free-Range Egg')
