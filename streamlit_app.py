df8b6f66c14372fa709ee94066f3dd7b34cd6fc7
MariaLunaC <123782711+MariaLunaC@users.noreply.github.com>
2023-01-30 14:00:45 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,4 +6,5 @@ streamlit.title('🐔 Omega 3 & Blueberry Oatmeal')
 streamlit.title('🥑 Kale, Spinach & Rocket Smoothie')
 streamlit.title('🍞 Hard-Boiled Free-Range Egg')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
