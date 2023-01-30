fa246435cf5e756ba6a939705ef49bf6bf7f73b0
MariaLunaC <123782711+MariaLunaC@users.noreply.github.com>
2023-01-30 13:57:04 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit
 streamlit.title('🥣 My Parents New Healthy Diner')
 streamlit.title('🥗 Breakfast Menu')
 streamlit.title('🐔 Omega 3 & Blueberry Oatmeal')
-streamlit.title('🥑 Kale, Spinach & Rocket Smoothie')
+streamlit.title('🥑 Kale, Spinach & Rocket Smoothie      ')
 streamlit.title('🍞 Hard-Boiled Free-Range Egg')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
