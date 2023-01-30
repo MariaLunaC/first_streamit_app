0d1358335ae0631657793f1c20b4b1bbd0380325
MariaLunaC <123782711+MariaLunaC@users.noreply.github.com>
2023-01-30 17:10:53 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,11 @@
 
 import streamlit
-streamlit.title('🥣 My Parents New Healthy Diner')
-streamlit.title('🥗 Breakfast Menu')
-streamlit.title('🐔 Omega 3 & Blueberry Oatmeal')
-streamlit.title('🥑 Kale, Spinach & Rocket Smoothie')
-streamlit.title('🍞 Hard-Boiled Free-Range Egg')
+streamlit.header(' My Parents New Healthy Diner')
+streamlit.title(' Breakfast Menu')
+streamlit.title('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.title('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.title('🐔 Hard-Boiled Free-Range Egg')
+streamlit.title('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
